649b824a5507b42fa48f8a85f83d43d0f097e6ca
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2025-01-14 13:46:09 +0100

    add and diplay employee skills

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -10,8 +10,24 @@ namespace EmployeeManagement
             Employee emp1 = new("john doe", 50000, new DateTime(2020, 1, 15));
             Employee emp2 = new("jane smith", 60000, new DateTime(2018, 5, 25));
 
+            emp1.AddSkill("c#");
+            emp1.AddSkill("sql");
+            emp1.AddSkill("javascript");
+
+            emp2.AddSkill("python");
+            emp2.AddSkill("django");
+            emp2.AddSkill("Machine learning");
+
+
             Console.WriteLine(emp1);
+            Console.WriteLine($"{emp1.GetName()}'skills");
+            emp1.DisplaySkills();
+
             Console.WriteLine(emp2);
+            Console.WriteLine($"{emp2.GetName()}'skills");
+            emp2.DisplaySkills();
+
+
 
             emp1.PayRaise(5000m);
             emp2.PayRaise(7000m);
