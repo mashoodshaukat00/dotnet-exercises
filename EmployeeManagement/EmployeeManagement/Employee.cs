57f329529d018d3e46cf23d5ae5e20090b3183fe
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2025-01-14 13:03:44 +0100

    add employee name , salary and join date.  Raise employee salary and test.

---
+++ EmployeeManagement/EmployeeManagement/Employee.cs
@@ -0,0 +1,55 @@
+﻿namespace EmployeeManagement
+{
+    public class Employee
+    {
+        private string name;
+        private decimal salary;
+        private DateTime joinDate;
+
+        public Employee(string name, decimal salary, DateTime joinDate)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.joinDate = joinDate;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public decimal GetSalary ()
+        {
+            return salary;
+        }
+
+        public void SetSalary (decimal salary)
+        {
+            this.salary = salary;
+        }
+
+        public DateTime GetJoinDate() 
+        { 
+            return joinDate;        
+        }
+
+        public void SetJoinDate(DateTime joinDate)
+        {
+            this.joinDate = joinDate;
+        }
+
+        public void PayRaise(decimal amount)
+        {
+            salary += amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Salary: {salary}, join date: {joinDate}";
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace EmployeeManagement
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Employee emp1 = new("john doe", 50000, new DateTime(2020, 1, 15));
+            Employee emp2 = new("jane smith", 60000, new DateTime(2018, 5, 25));
+
+            Console.WriteLine(emp1);
+            Console.WriteLine(emp2);
+
+            emp1.PayRaise(5000m);
+            emp2.PayRaise(7000m);
+
+            Console.WriteLine(emp1);
+            Console.WriteLine(emp2);
+
+        }
+    }
+}
