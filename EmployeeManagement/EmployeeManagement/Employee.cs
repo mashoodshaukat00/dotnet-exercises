a919782c6b439431a028e346a2eac0fe2a1ec3f4
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2025-01-14 13:20:28 +0100

    define functions to add skill and display skills

--- EmployeeManagement/EmployeeManagement/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Employee.cs
@@ -6,11 +6,18 @@
         private decimal salary;
         private DateTime joinDate;
 
+        private string[] skills;
+        private int skillCount;
+        private const int MaxSkills = 10;
+
         public Employee(string name, decimal salary, DateTime joinDate)
         {
             this.name = name;
             this.salary = salary;
             this.joinDate = joinDate;
+
+            this.skills = new string[MaxSkills];
+            this.skillCount = 0;
         }
         public string GetName()
         {
@@ -45,6 +52,28 @@
         public void PayRaise(decimal amount)
         {
             salary += amount;
+        }
+
+        public void AddSkill(string skill)
+        {
+            if(skillCount < MaxSkills)
+            {
+                skills[skillCount] = skill;
+                skillCount++;
+            }
+            else
+            {
+                Console.WriteLine("Cannot add more skills. Maximum limit reached.");
+            }
+        }
+
+        public void DisplaySkills()
+        {
+            Console.WriteLine("Skills:");
+            for(int i = 0; i < skillCount; i++)
+            {
+                Console.WriteLine($"-{skills[i]}");
+            }
         }
 
         public override string ToString()
